f4a68a578339af21c5295f57a4b81c92a34007dc
Capt. Michael <CaptMichaelDev@gmail.com>
2025-02-22 12:07:45 -0500

    perf: config of deno fmt

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ const entryPoint = (req: Request): Response => {
   const url = new URL(req.url);
   const params = new URLSearchParams(url.search);
 
-  const name = params.get("name") || "DoneSaaS";
+  const name = params.get('name') || 'DoneSaaS';
   const responseText = `Hello, ${name}!`;
 
   return new Response(responseText, {
     status: 200,
     headers: {
-      "content-type": "application/json",
+      'content-type': 'application/json',
     },
   });
 };
